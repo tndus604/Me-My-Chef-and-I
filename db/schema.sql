894d06c1ddca4f4869423fcaa7164327ccc28a1b
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-03 13:04:02 -0400

    set the router

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ USE fridge_db;
 
 CREATE TABLE food (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category ENUM('vegetable', 'fruit', 'dairy', 'meat', 'fish', 'others') DEFAULT 'others',
+    category ENUM('vegetable', 'fruit', 'dairy', 'meat', 'seafood', 'others') DEFAULT 'others',
     item VARCHAR(50) NOT NULL,
     is_rotten BOOLEAN DEFAULT FALSE,
+    quantity INT DEFAULT 5,
     image_url TEXT NOT NULL
 );
