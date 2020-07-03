161fbaad5a30dab8888c70602c2ec0b052dd1286
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-07-03 13:13:54 -0400

    merge

---
+++ db/seed.sql
@@ -0,0 +1,35 @@
+INSERT INTO food (category, item, image_url) VALUES 
+("seafood", "Salmon", "https://gamepedia.cursecdn.com/arksurvivalevolved_gamepedia/9/98/Raw_Prime_Fish_Meat.png"),
+("seafood", "Smoked Salmon", "https://ui.assets-asda.com/dm/asdagroceries/5054070266014_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=RYOR70&fmt=jpg&fit=constrain,1&wid=256&hei=256"),
+("seafood", "Mussels", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDGAw2WlrSAEer1UoW2zQZft-VB3USnjdzCA&usqp=CAU"),
+("seafood", "Crab", "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/3145715/36ae9d0849ee3ab3f79eac13399f2d07_large.png&width=256&type=webp&quality=40"),
+("seafood", "Lobster", "https://pbs.twimg.com/profile_images/3223466311/25a75aa99f4c835ded6dfe580326a1ff.jpeg"),
+("seafood", "Shrimp", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSTFzkARQ4NLt7YuSAOW92CzQGm_QvouR-wTg&usqp=CAU"),
+("seafood", "Oyster", "https://pbs.twimg.com/profile_images/378800000305910672/2cf9161c5db6aca9f60c483153538887.jpeg"),
+("seafood", "Clam", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBzUsrNlChlNFu0bui81FTP4U8vn-mOFFPMw&usqp=CAU"),
+("seafood", "Scallop", "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/7733967/391a55df354191213c5d4d05ef21aa58_large.png&width=256&type=webp&quality=40"),
+("seafood", "Squid", "https://images.freshop.com/7086765/0b0c3213bc5aa54591c9b133b52cb15c_medium.png")
+
+INSERT INTO food (category, item, image_url) VALUES 
+("vegetable", "Onion", "https://ui.assets-asda.com/dm/asdagroceries/0000000005883_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=xOhSb0&fmt=jpg&fit=constrain,1&wid=256&hei=256"),
+("vegetable", "Green Onion", "https://images.freshop.com/8215695/c6c61545ead2df52626bc5a7c4525aec_medium.png"),
+("vegetable", "Carrots", "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/193517/c8d02d906bb4cafa0b81d6e0fdf2a495_large.png&width=256&type=webp&quality=40"),
+("vegetable", "Mushrooms", "https://vignette.wikia.nocookie.net/ark-soyo/images/1/16/PsilocybinMushrooms_Icon_SPlus_preview.png/revision/latest?cb=20171205065850"),
+("vegetable", "Broccoli", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJWzg9pRnlh_AiVYTdzmvEnAEXqlEs0oRybA&usqp=CAU"),
+("vegetable", "Celery", "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/2610643/7f5c603bcde7d945074e2495cba7b55f_large.png&width=256&type=webp&quality=40"),
+("vegetable", "Tomatoes", "https://pbs.twimg.com/profile_images/378800000549764122/52f63b2ccdbfdc4e4993559f02078fd4.jpeg"),
+("vegetable", "Cucumber", "https://a.wattpad.com/useravatar/peerlesscucumber.256.646114.jpg"),
+("vegetable", "Basil", "https://images.vexels.com/media/users/3/149059/isolated/lists/570fb153da3b8532e24364206fcc80b9-basil-herb-illustration.png"),
+("vegetable", "Garlic", "https://pbs.twimg.com/profile_images/723146614114779136/ZNuJHu5t_400x400.jpg")
+
+INSERT INTO food (category, item, image_url) VALUES
+("others", "Bread", "https://gamepedia.cursecdn.com/arksurvivalevolved_gamepedia/9/9f/Baked_Bread_Loaf_%28Primitive_Plus%29.png"),
+("others", "Salt", "https://img.favpng.com/5/24/3/glass-bottle-lid-mason-jar-how-to-png-favpng-BFhMVwLHcyRqssDVhV8qHUiAM_t.jpg"),
+("others", "Pepper", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtBfwMYJ9n0vv9nPWwG8wSOVXJzjCQ_LCLjA&usqp=CAU"),
+("others", "Cooking Oil", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmEsr2Cg-Tn4LVpEtywitiQi05SiElYrKa4Q&usqp=CAU"),
+("others", "Vinegar", "https://cdn.fratelliguzzini.filoblu.com/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/1/6/16960100_1.jpg"),
+("others", "Sugar", "https://b3h2.scene7.com/is/image/BedBathandBeyond/40824941884423p?$imagePLP$&wid=256&hei=256"),
+("others", "Pasta", "https://images.freshop.com/2596663/bd2c4f9bc59d6bc3baad7f38cfd42dc1_medium.png"),
+("others", "Rice Noodles", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRaftmNJRlBwd6aJW3TR1r09YHySAZRhHKOCQ&usqp=CAU"),
+("others", "Ketchup", "https://ui.assets-asda.com/dm/asdagroceries/5000157140241_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=CTFSN0&fmt=jpg&fit=constrain,1&wid=256&hei=256"),
+("others", "Mayonnaise", "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00079801246788/39af1f02be11dc6ae95e3917e60659c5_large.png&width=256&type=webp&quality=40")
