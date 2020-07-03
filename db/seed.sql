f231438c1f8b569fe3575a309f83a3f47ad8d824
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-02 20:56:04 -0400

    created basic wireframe

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,1 @@
-INSERT INTO food (items, quantity) VALUES ("onion", "4"), ("egg", "6"), ("cheese","10");
+INSERT INTO food (category, item) VALUES ("vegetable", "onion"), ("vegetable", "green onion"), ("vegetable","carrot");
